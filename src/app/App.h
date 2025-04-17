5f75e4cb302a596a0c11deed4b3367c6d43317bb
nav1s <42621369+nav1s@users.noreply.github.com>
2025-04-17 15:35:58 +0300

    Update App.h

--- src/app/App.h
+++ src/app/App.h
@@ -6,28 +6,30 @@
 #include <string>
 #include <memory>
 
-#include "C:\BarIlan\Year3\AdvancedPrograming\Exercise1\project\src\command\ICommand.h"
-#include "C:\BarIlan\Year3\AdvancedPrograming\Exercise1\project\src\filter\IFilter.h"
-#include "C:\BarIlan\Year3\AdvancedPrograming\Exercise1\project\src\menu\IMenu.h"
+#include "ICommand.h"
+#include "IFilter.h"
+#include "IMenu.h"
+
+using namespace std;
 
 // Represents the main application class that controls the flow of commands and menu
 class App {
 private:
     // Maps menu options to corresponding commands
-    std::map<int, std::shared_ptr<ICommand>> commands;
+    map<int, std::shared_ptr<ICommand>> commands;
 
     // Which filter used for storing/querying URLs
-    std::shared_ptr<IFilter> filter;
+    shared_ptr<IFilter> filter;
 
     // The menu interface (used for interaction with user)
-    std::shared_ptr<IMenu> menu;
+    shared_ptr<IMenu> menu;
 
 public:
     // Constructor initializing the app with a filter and menu implementation
-    App(std::shared_ptr<IFilter> filter, std::shared_ptr<IMenu> menu);
+    App(shared_ptr<IFilter> filter, shared_ptr<IMenu> menu);
 
     // Registers a new command to a specific option in the menu
-    void registerCommand(int option, std::shared_ptr<ICommand> command);
+    void registerCommand(int option, shared_ptr<ICommand> command);
 
     // Runs the main application loop
     void run();
