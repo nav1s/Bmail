9462569ef17da3b042d594a2e998b55e106dd1bd
root <root@localhost>
2025-05-02 10:11:10 +0300

    add rule of 5 for TCPReader

--- src/input/TCPReader.cpp
+++ src/input/TCPReader.cpp
@@ -1,6 +1,9 @@
 #include <string>
 #include "TCPReader.h"
 
+TCPReader::TCPReader(int clientSocket) : clientSocket(clientSocket), buffer() {
+    memset(buffer, 0, sizeof(buffer));
+  }
 bool TCPReader::getLine(std::string &line) {
   // Read a line from the TCP socket
   ssize_t bytes_received = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
--- src/input/TCPReader.h
+++ src/input/TCPReader.h
@@ -1,20 +1,52 @@
 #pragma once
 
 #include "InputReader.h"
+#include <cstring>
 #include <sys/socket.h>
 
-class TCPReader: public InputReader {
+class TCPReader : public InputReader {
 public:
+  /**
+   * @brief Constructs a TCPReader with the specified socket.
+   *
+   * @param clientSocket The socket to read from.
+   */
+  TCPReader(int clientSocket);
 
-    /**
-     * @brief gets a line from the TCP socket.
-     * 
-     * @param string& line The string to store the line in.
-     * @return true if successful, false if an error occurred or the connection was closed.
-     */
-    bool getLine(std::string &line) override;
+  /**
+   * @brief Deleted copy constructor.
+   */
+  TCPReader(const TCPReader &other) = delete;
+
+  /**
+   * @brief Deleted copy assignment operator.
+   */
+  TCPReader &operator=(const TCPReader &other) = delete;
+
+  /**
+  * brief Deleted move constructor.
+   */
+  TCPReader(TCPReader &&other) = delete;
+
+  /**
+   * @brief Deleted move assignment operator.
+   */
+  TCPReader &operator=(TCPReader &&other) = delete;
+
+  /**
+   * @brief Destructor
+   */
+  ~TCPReader();
+
+  /**
+   * @brief gets a line from the TCP socket.
+   *
+   * @param string& line The string to store the line in.
+   * @return true if successful, false if an error occurred or the connection was closed.
+   */
+  bool getLine(std::string &line) override;
 
 private:
-    int clientSocket;
-    char buffer[1024];
+  int clientSocket;
+  char buffer[1024];
 };
