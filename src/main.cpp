f27e0fa97813d3e29e6ba83298c75502afa910dd
root <root@localhost>
2025-05-02 16:29:13 +0300

    add server implementation to the cpp code

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,30 @@
 #include "app/App.h"
-#include "input/CliReader.h"
-#include "output/CliPrinter.h"
+#include "input/TCPReader.h"
+#include "network/TCPServer.h"
+#include "output/TCPWriter.h"
+#include <iostream>
 
-int main() {
+int main(int argc, char* argv[]) {
+    // Check if the correct number of arguments is provided
+    if (argc != 3) {
+        std::cerr << "Usage: ./tcp-server ip_address port" << std::endl;
+        return 1;
+    }
+
+    std::string ip_address = argv[1];
+    std::string port = argv[2];
+
+    std::cout << "IP Address: " << ip_address << std::endl;
+    std::cout << "Port: " << port << std::endl;
+    TCPServer server(ip_address, std::stoi(port));
+    server.initializeServer();
+    int clientSocket = server.acceptConnection();
+
+    TCPReader reader(clientSocket);
+    TCPWriter writer(clientSocket);
+
     App app;
-    CliReader reader;
-    CLIPrinter printer;
-    app.run(reader, printer);
+    app.run(reader, writer);
 
     return 0;
 }
