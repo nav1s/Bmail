8d1302daffc63d05168688142022d9ef856c0940
root <root@localhost>
2025-05-21 15:52:42 +0300

    add initial node files

---
+++ src/webServer/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+var app = express();
+app.use(express.json());
+
+const users = require('./routes/users');
+app.use('/api/users', users);
+
+const tokens = require('./routes/tokens');
+app.use('/api/tokens', tokens);
+
+const mails = require('./routes/mails');
+app.use('/api/mails', mails);
+
+const labels = require('./routes/labels');
+app.use('/api/labels', labels);
+
+const blacklist = require('./routes/blacklist');
+app.use('/api/blacklist', blacklist);
+
+const search = require('./routes/search');
+app.use('/api/search', search);
+
+app.listen(8080)
