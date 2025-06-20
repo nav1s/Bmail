d59101f287d78b1733347c30df1633b0c1bdb02b
root <root@localhost>
2025-06-20 10:59:58 +0300

    add defaultLabelsTests.test.js (only todos for now)

---
+++ src/webServer/tests/defaultLabelsTests.test.js
@@ -0,0 +1,8 @@
+
+// todo add test that mail is assigned to the sender's sent labels
+// todo add test that the mail is assigned to the recipient's inbox label
+// todo add test that when mail is starred it is assigned to the starred label
+// todo add test that drafts are assigned to the drafts label
+// todo add test that draft label are unassigned when mail is sent
+
+// todo add test for /api/username/:username route
