e40907c2decff4fd568ec078e43cc1feabefc1ad
root <root@localhost>
2025-06-23 08:57:40 +0300

    enable access to upload dir

--- src/webServer/app.js
+++ src/webServer/app.js
@@ -4,10 +4,13 @@ const tokens = require('./routes/tokens');
 const mails = require('./routes/mails');
 const labels = require('./routes/labels');
 const blacklist = require('./routes/blacklist');
+const path = require('path');
 const { serverError, notFound } = require('./utils/httpResponses');
 
 const app = express();
 app.use(express.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 const cors = require("cors");
 
