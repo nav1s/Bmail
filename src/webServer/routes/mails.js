9b84070639632f705cff0e40d155c93f84f9766f
root <root@localhost>
2025-06-24 15:06:43 +0300

    remove a fixed todo

--- src/webServer/routes/mails.js
+++ src/webServer/routes/mails.js
@@ -25,7 +25,6 @@ router.get('/search/:query', requireAuth, searchMails);
 router.get('/:id', requireAuth, getMailById);
 router.patch('/:id', requireAuth, updateMailById);
 
-// todo fix route
 router.delete('/:id', requireAuth, deleteMailById);
 
 module.exports = router;
