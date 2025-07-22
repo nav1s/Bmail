c134184bb305ad0f348b23b049f44ecec9a7fc19
root <root@localhost>
2025-07-22 18:19:36 +0300

    implement signup button changing the activity to the signup activity

--- src/android/app/src/main/java/com/example/bmail/LoginActivity.java
+++ src/android/app/src/main/java/com/example/bmail/LoginActivity.java
@@ -1,13 +1,22 @@
 package com.example.bmail;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button signupBtn = findViewById(R.id.signup);
+        signupBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        });
+
 
     }
 }
