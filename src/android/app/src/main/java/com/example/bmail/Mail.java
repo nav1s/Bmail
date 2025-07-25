1e87fd65e1d137ec1f342c1939d097635976bb75
root <root@localhost>
2025-07-25 11:15:24 +0300

    add Mail entity for Room database integration

---
+++ src/android/app/src/main/java/com/example/bmail/Mail.java
@@ -0,0 +1,16 @@
+package com.example.bmail;
+
+import androidx.room.Entity;
+
+import java.util.List;
+
+@Entity
+public class Mail {
+    @PrimaryKey
+    public int id;
+    public String subject;
+    public String body;
+    public String sender;
+    public List<String> recipients;
+    public Boolean draft;
+}
