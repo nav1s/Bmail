afb51fbeb7558281477047ad80cd5afbd17a8cb9
root <root@localhost>
2025-07-25 11:36:57 +0300

    refactor Mail entity to encapsulate fields and add getter/setter methods

--- src/android/app/src/main/java/com/example/bmail/Mail.java
+++ src/android/app/src/main/java/com/example/bmail/Mail.java
@@ -8,10 +8,58 @@ import java.util.List;
 @Entity
 public class Mail {
     @PrimaryKey
-    public int id;
-    public String subject;
-    public String body;
-    public String sender;
-    public List<String> recipients;
-    public Boolean draft;
+    private final int id;
+    private String subject;
+    private String body;
+    private String sender;
+    private List<String> recipients;
+    private Boolean draft;
+
+    public Mail(int id) {
+        this.id = id;
+    }
+
+    public Boolean getDraft() {
+        return draft;
+    }
+
+    public void setDraft(Boolean draft) {
+        this.draft = draft;
+    }
+
+    public List<String> getRecipients() {
+        return recipients;
+    }
+
+    public void setRecipients(List<String> recipients) {
+        this.recipients = recipients;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
