d2046f77aad616945cc91d12c45dda9be87688b9
root <root@localhost>
2025-07-25 11:20:17 +0300

    refactor MailRepository to implement LiveData for mail list management

--- src/android/app/src/main/java/com/example/bmail/MailRepository.java
+++ src/android/app/src/main/java/com/example/bmail/MailRepository.java
@@ -3,6 +3,11 @@ package com.example.bmail;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.LinkedList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -10,25 +15,42 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MailRepository {
-    private final MailsApi mailsApi;
-    private final Context context;
+    static class MailListData extends MutableLiveData<List<Mail>> {
+        public MailListData() {
+            super();
+            List<Mail> mails = new LinkedList<>();
+            setValue(mails);
+        }
+    }
+    private final MailListData mailListData = new MailListData();
 
-    public MailRepository(Context context) {
-        this.context = context.getApplicationContext();
+//    private final MailsApi mailsApi;
+//    private final Context context;
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://localhost:8080/api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        mailsApi = retrofit.create(MailsApi.class);
+    public MailRepository(@NonNull Context context) {
+//        this.context = context.getApplicationContext();
+//
+//        Retrofit retrofit = new Retrofit.Builder()
+//                .baseUrl("http://localhost:8080/api/")
+//                .addConverterFactory(GsonConverterFactory.create())
+//                .build();
+//        mailsApi = retrofit.create(MailsApi.class);
     }
 
-    // Dummy method to simulate loading emails
-    public void getEmails() {
-        SharedPreferences prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
-        String token = prefs.getString("auth_token", null);
+    public LiveData<List<Mail>> getMails() {
+        return mailListData;
+//        SharedPreferences prefs = context.getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
+//        String token = prefs.getString("auth_token", null);
+//
+//        Call<List<MailResponse>> call = mailsApi.getMails(token);
+    }
 
-        Call<List<MailResponse>> call = mailsApi.getMails(token);
+    public void sendMail(Mail mail) {
+    }
+    public void deleteMail(Mail mail) {
+    }
+    public void reloadMails(){
+
     }
 }
 
