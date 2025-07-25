50290e7b011776b485a865f697e6d2d72a305841
root <root@localhost>
2025-07-25 11:21:58 +0300

    refactor MailViewModel to use LiveData for mail management and add methods for adding, deleting, and loading mails

--- src/android/app/src/main/java/com/example/bmail/MailViewModel.java
+++ src/android/app/src/main/java/com/example/bmail/MailViewModel.java
@@ -1,21 +1,31 @@
 package com.example.bmail;
 
+import androidx.lifecycle.LiveData;
+
 import java.util.List;
 
-public class MailViewModel {
+public class MailViewModel extends androidx.lifecycle.ViewModel {
     private final MailRepository mailRepository;
-    private List<String> emails;
+    private final LiveData<List<Mail>> mails;
 
     public MailViewModel(MailRepository mailRepository) {
         this.mailRepository = mailRepository;
+        mails = mailRepository.getMails();
     }
 
-    public void loadEmails() {
-        mailRepository.getEmails();
+    public LiveData<List<Mail>> getMails() {
+        return mails;
     }
 
-    public List<String> getEmails() {
-        return emails;
+
+    public void add(Mail mail) {
+        mailRepository.sendMail(mail);
+    }
+
+    public void delete(Mail mail) {
+        mailRepository.deleteMail(mail);
+    }
+    public void loadMails() {
+        mailRepository.reloadMails();
     }
 }
-
