42ae5a8ef81962ab382753efa100c66011abe999
root <root@localhost>
2025-07-29 12:21:15 +0300

    update ProfileActivity: add new activity with basic setup and layout

---
+++ src/android/app/src/main/java/com/example/bmail/Activities/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.example.bmail.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bmail.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+    }
+
+}
