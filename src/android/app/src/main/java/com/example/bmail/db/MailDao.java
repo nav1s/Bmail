add69d1dba620b875bc76870706d0968f81ac748
root <root@localhost>
2025-07-30 17:03:54 +0300

    feat(MailDao): create MailDao interface for database operations

---
+++ src/android/app/src/main/java/com/example/bmail/db/MailDao.java
@@ -0,0 +1,28 @@
+package com.example.bmail.db;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.bmail.Entities.Mail;
+
+import java.util.List;
+
+@Dao
+public interface MailDao {
+    @Query("SELECT * from mail")
+    List<Mail> index();
+    @Query("SELECT * from mail WHERE id = :id")
+    Mail getById(int id);
+
+    @Insert
+    void insert(Mail mail);
+    @Update
+    void update(Mail mail);
+    @Delete
+    void delete(Mail mail);
+
+
+}
