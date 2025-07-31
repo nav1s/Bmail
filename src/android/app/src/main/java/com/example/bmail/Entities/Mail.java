8c3c984fb2fc774c91c2a767d95a5ec3d2496d6c
root <root@localhost>
2025-07-31 13:02:25 +0300

    refactor(Mail): remove unused ColumnInfo import

--- src/android/app/src/main/java/com/example/bmail/Entities/Mail.java
+++ src/android/app/src/main/java/com/example/bmail/Entities/Mail.java
@@ -1,7 +1,6 @@
 package com.example.bmail.Entities;
 
 import androidx.annotation.NonNull;
-import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 import com.google.gson.annotations.Expose;
