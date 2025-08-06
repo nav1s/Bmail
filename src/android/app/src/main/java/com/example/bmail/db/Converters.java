be2b3259b48af200b72687ad6358d87798e93ca3
root <root@localhost>
2025-08-06 12:58:45 +0300

    feat: add TypeConverters for List<Integer> serialization and deserialization

--- src/android/app/src/main/java/com/example/bmail/db/Converters.java
+++ src/android/app/src/main/java/com/example/bmail/db/Converters.java
@@ -27,4 +27,23 @@ public class Converters {
         Gson gson = new Gson();
         return gson.toJson(list);
     }
+
+    @TypeConverter
+    public static String fromIntegerList(List<Integer> list) {
+        if (list == null) {
+            return null;
+        }
+        Gson gson = new Gson();
+        return gson.toJson(list);
+    }
+
+    @TypeConverter
+    public static List<Integer> fromStringToIntegerList(String value) {
+        if (value == null) {
+            return null;
+        }
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<Integer>>() {}.getType();
+        return gson.fromJson(value, listType);
+    }
 }
