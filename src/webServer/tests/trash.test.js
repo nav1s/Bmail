1c965aed385210917e3548817d754648aa41311b
nav1s <42621369+nav1s@users.noreply.github.com>
2025-10-01 10:35:38 +0300

    refactor: trash mail tests to use DB setup and teardown and fix broken tests

--- src/webServer/tests/trash.test.js
+++ src/webServer/tests/trash.test.js
@@ -1,11 +1,22 @@
-const { test } = require('node:test');
+const { after, before, test } = require("node:test");
+const mongoose = require("mongoose");
+const config = require("../utils/config");
+const User = require("../models/usersModel");
+const Mail = require("../models/mailsModel");
+const { Label } = require("../models/labelsModel");
 const assert = require('node:assert/strict');
 const supertest = require('supertest');
 const app = require('../app');
 const api = supertest(app);
 
-let nonAttachableLabels;
-let trashLabelId;
+let nonAttachableLabels, trashLabelId, token, mailId
+
+before(async () => {
+  await mongoose.connect(config.MONGODB_URI);
+  await User.deleteMany({});
+  await Mail.deleteMany({});
+  await Label.deleteMany({});
+});
 
 // 0. Setup
 test('0. Setup: Register, login, create label and mail', async () => {
@@ -23,11 +34,12 @@ test('0. Setup: Register, login, create label and mail', async () => {
         .send({ username: 'testUser', password: securePass })
         .expect(201);
     token = login.body.token;
+    console.log("token: " + token)
 
     // Create mail
     const mailRes = await api.post('/api/mails')
         .set('Authorization', 'bearer ' + token)
-        .send({ to: ['testUser'], title: 'Hello', body: 'world' })
+        .send({ to: ['testUser@bmail.com'], title: 'Hello', body: 'world' })
         .expect(201);
     mailId = mailRes.body.id;
 
@@ -50,12 +62,13 @@ test('0. Setup: Register, login, create label and mail', async () => {
     console.log('Trash Label ID:', trashLabelId);
 });
 
+
 // add trash label to mail
 test('Add trash label to mail', async () => {
     await api.post(`/api/mails/${mailId}/labels`)
         .set('Authorization', 'bearer ' + token)
         .send({ labelId: trashLabelId })
-        .expect(204);
+        .expect(200);
 
     // Verify the mail has the trash label
     const mailRes = await api.get(`/api/mails/${mailId}`)
@@ -84,7 +97,7 @@ test('List inbox should not include trash mails', async () => {
 });
 
 // list mails by trash label should include the mail
-test('List mails by trash label should include the mail', async () => { 
+test('List mails by trash label should include the mail', async () => {
     const res = await api.get('/api/mails/byLabel/trash')
         .set('Authorization', 'bearer ' + token)
         .expect(200);
@@ -99,7 +112,7 @@ test('List mails by trash label should include the mail', async () => {
 test('Remove trash label from mail', async () => {
     await api.delete(`/api/mails/${mailId}/labels/${trashLabelId}`)
         .set('Authorization', 'bearer ' + token)
-        .expect(204);
+        .expect(200);
 
     // Verify the mail has no trash label
     const mailRes = await api.get(`/api/mails/${mailId}`)
@@ -124,7 +137,7 @@ test('List inbox should include the mail again', async () => {
 });
 
 // list mails by trash label should not include the mail
-test('List mails by trash label should not include the mail', async () => { 
+test('List mails by trash label should not include the mail', async () => {
     const res = await api.get('/api/mails/byLabel/trash')
         .set('Authorization', 'bearer ' + token)
         .expect(200);
@@ -133,3 +146,7 @@ test('List mails by trash label should not include the mail', async () => {
     // check if the mail is not in the trash label
     assert(!mails.some(mail => mail.id === mailId), 'trash label should not include the mail');
 });
+
+ after(async () => {
+   await mongoose.connection.close();
+ });
